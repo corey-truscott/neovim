e2971951ecbd5dab1d4d49c658a955611920e0bb
corey-truscott <coreytruscott8@tutanota.com>
2024-04-17 21:19:31 +0100

    transparent

--- lua/plugin/colourscheme.lua
+++ lua/plugin/colourscheme.lua
@@ -1,7 +1,15 @@
 return {
-	"sainnhe/everforest",
-	priority = 1000,
-	init = function()
-		vim.cmd.colorscheme("everforest")
-	end,
+    {
+        "sainnhe/everforest",
+        priority = 1000,
+        init = function()
+            vim.cmd.colorscheme("everforest")
+        end,
+    },
+    {
+        "xiyaowong/transparent.nvim",
+        config = function()
+            require("transparent").setup({})
+        end,
+    },
 }
