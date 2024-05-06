5a3de14042aa908a906d1b10e1f672bfccd9b067
Corey Truscott <coreytruscott8@gmail.com>
2024-05-06 05:14:40 +0100

    move abbreviation to after

---
+++ after/ftplugin/go.lua
@@ -0,0 +1,3 @@
+-- :ab ifer if err != nil {<CR>return err<CR>}<ESC>kw
+-- i dont think theres a way to do this with lua directly
+vim.cmd("ab ifer if err != nil {<CR>return err<CR>}<ESC>kw")
--- lua/opt/set.lua
+++ lua/opt/set.lua
@@ -87,7 +87,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
-
--- :ab ifer if err != nil {<CR>return err<CR>}<ESC>kw
--- i dont think theres a way to do this with lua directly
-vim.cmd("ab ifer if err != nil {<CR>return err<CR>}<ESC>kw")
