e93e32ac8f86814e70440cba70e23337ab687ebd
Corey Truscott <coreytruscott8@gmail.com>
2024-05-06 02:37:03 +0100

    add keybinds for markdown preview and add keybind

---
+++ lua/plugin/markdown-preview.lua
@@ -0,0 +1,15 @@
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function()
+        vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+        {
+            "<leader>mt",
+            "<CMD>MarkdownPreviewToggle<CR>",
+            desc = "Toggle markdown preview",
+        },
+    },
+}
