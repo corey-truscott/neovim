5e9549e5aacd5dc56ff2617be3db6c6d31847a65
Corey Truscott <coreytruscott8@gmail.com>
2024-05-17 19:29:48 +0100

    check neovim version

--- init.lua
+++ init.lua
@@ -1,13 +1,23 @@
--- bootstrap lazy
+if vim.fn.has("nvim-0.9.0") == 0 then
+  vim.api.nvim_echo({
+    { "Configuration requires Neovim >= 0.9.0\n", "ErrorMsg" },
+    { "Press any key to exit", "MoreMsg" },
+  }, true, {})
+  vim.fn.getchar()
+  vim.cmd([[quit]])
+  return {}
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazyrepo = "https://github.com/folke/lazy.nvim.git"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	print("Downloading lazy.nvim, please wait")
 	vim.fn.system({
 		"git",
 		"clone",
 		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
+		lazyrepo,
+		"--branch=stable",
 		lazypath,
 	})
 end
