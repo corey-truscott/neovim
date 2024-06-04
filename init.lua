962dc7e753292a7f553efe7db6d727b513cf53d5
Corey Truscott <coreytruscott8@gmail.com>
2024-06-04 22:05:59 +0100

    variable names for trouble keybinds

--- init.lua
+++ init.lua
@@ -1,10 +1,8 @@
 if vim.fn.has("nvim-0.10.0") == 0 then
+    -- stylua: ignore
     vim.api.nvim_echo({
         { "the configuration requires neovim version >= 0.10.0\n", "ErrorMsg" },
-        {
-            "update to a newer version of neovim\n",
-            "WarningMsg",
-        },
+        { "update to a newer version of neovim\n",                 "WarningMsg", },
         { "press any key to exit...",                              "MoreMsg" },
     }, true, {})
 
--- lua/plugin/trouble.lua
+++ lua/plugin/trouble.lua
@@ -3,57 +3,77 @@ return {
     cmd = { "Trouble" },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
+        focus = true,
         indent_guides = true,
         follow = true,
         auto_refresh = true,
         auto_close = false,
         use_diagnostic_signs = true,
     },
-    keys = {
-        {
-            "<leader>tt",
-            function()
-                vim.cmd("Trouble diagnostics toggle")
-            end,
-            desc = "Trouble",
-        },
 
-        {
-            "<leader>tT",
-            function()
-                vim.cmd("Trouble diagnostics toggle filter.buf=0")
-            end,
-            desc = "Trouble (Single buffer)",
-        },
+    keys = function()
+        local keys = {}
 
-        {
-            "[q",
-            function()
-                if require("trouble").is_open() then
-                    require("trouble").prev({ skip_groups = true, jump = true })
-                else
-                    local ok, err = pcall(vim.cmd.cprev)
-                    if not ok then
-                        vim.notify(err, vim.log.levels.ERROR)
-                    end
+        --- add a key mapping.
+        --- @param map string: the key mapping
+        --- @param command function | string: the function or command to execute
+        --- @param description string: the description of the key mapping
+        --- @param extension boolean: add " (Trouble)" to the end
+        local add_key = function(map, command, description, extension)
+            if extension then
+                description = description .. " (Trouble)"
+            end
+            table.insert(keys, {
+                map,
+                command,
+                desc = description,
+            })
+        end
+
+        add_key("<leader>tt", function()
+            require("trouble").toggle({
+                mode = "diagnostics",
+                filter = { nil },
+            })
+        end, "Diagnostics", true)
+
+        add_key("<leader>tT", function()
+            require("trouble").toggle({
+                mode = "diagnostics",
+                filter = { buf = vim.api.nvim_get_current_buf() },
+            })
+        end, "Filtered diagnostics", true)
+
+        add_key("[q", function()
+            if require("trouble").is_open() then
+                ---@diagnostic disable-next-line: missing-parameter, missing-fields
+                require("trouble").prev({
+                    skip_groups = true,
+                    jump = true,
+                })
+            else
+                local ok, err = pcall(vim.cmd.cprev)
+                if not ok then
+                    vim.notify(err, vim.log.levels.ERROR)
                 end
-            end,
-            desc = "Previous Trouble/Quickfix Item",
-        },
+            end
+        end, "Previous Trouble/Quickfix item", false)
 
-        {
-            "]q",
-            function()
-                if require("trouble").is_open() then
-                    require("trouble").next({ skip_groups = true, jump = true })
-                else
-                    local ok, err = pcall(vim.cmd.cnext)
-                    if not ok then
-                        vim.notify(err, vim.log.levels.ERROR)
-                    end
+        add_key("]q", function()
+            if require("trouble").is_open() then
+                ---@diagnostic disable-next-line: missing-parameter, missing-fields
+                require("trouble").next({
+                    skip_groups = true,
+                    jump = true,
+                })
+            else
+                local ok, err = pcall(vim.cmd.cnext)
+                if not ok then
+                    vim.notify(err, vim.log.levels.ERROR)
                 end
-            end,
-            desc = "Next Trouble/Quickfix Item",
-        },
-    },
+            end
+        end, "Next Trouble/Quickfix item", false)
+
+        return keys
+    end,
 }
