77d655ad6bad42757f91bda4817b4d101d2aa872
Corey Truscott <coreytruscott8@gmail.com>
2024-06-05 14:32:19 +0100

    c main snippet

---
+++ after/ftplugin/c.lua
@@ -0,0 +1,28 @@
+-- require("luasnip.session.snippet_collection").clear_snippets "c"
+
+local ls = require("luasnip")
+
+local s = ls.snippet
+local i = ls.insert_node
+
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.add_snippets("c", {
+    s(
+        "m",
+        fmt(
+            [[
+int main(int argc, char *argv[]) {{
+    {}
+    return 0;
+}}
+]],
+            { i(1, "// main") }
+        )
+    ),
+    -- produces this:
+    -- int main(int argc, char *argv[]) {
+    --     // main
+    --     return 0;
+    -- }
+})
