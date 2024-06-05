e2d2c1ff41ce18fd4540a5c579b2ddcda0b71391
Corey Truscott <coreytruscott8@gmail.com>
2024-06-05 23:15:26 +0100

    gco and gcO keybinds

--- lua/opt/remap.lua
+++ lua/opt/remap.lua
@@ -43,6 +43,7 @@ vim.keymap.set("n", "<leader>[b", ":bprevious<CR>", { desc = "Previous buffer" }
 -- stay in visual mode when indenting with >> or <<
 vim.keymap.set("v", "<", "<gv", { desc = "Reindent while staying in visual mode" })
 vim.keymap.set("v", ">", ">gv", { desc = "Reindent while staying in visual mode" })
+-- stylua: ignore end
 
 local default_scrolloff = vim.o.scrolloff
 local alternate_scrolloff = 999
@@ -57,4 +58,35 @@ vim.keymap.set("n", "<leader>cc", function()
     end
 end, { desc = "Toggle centered cursor" })
 
--- stylua: ignore end
+local function comment_above_or_below(lnum)
+    local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+    local comment_row = row + lnum
+    local l_cms, r_cms = string.match(vim.bo.commentstring, "(.*)%%s(.*)")
+    l_cms = vim.trim(l_cms)
+    r_cms = vim.trim(r_cms)
+    if #r_cms ~= 0 then
+        r_cms = " " .. r_cms
+    end
+    vim.api.nvim_buf_set_lines(
+        0,
+        comment_row,
+        comment_row,
+        false,
+        { l_cms .. " " .. r_cms }
+    )
+    vim.api.nvim_win_set_cursor(0, { comment_row + 1, 0 })
+    vim.api.nvim_command("normal! ==")
+    vim.api.nvim_win_set_cursor(
+        0,
+        { comment_row + 1, #vim.api.nvim_get_current_line() - #r_cms - 1 }
+    )
+    vim.api.nvim_feedkeys("a", "ni", true)
+end
+
+vim.keymap.set("n", "gco", function()
+    comment_above_or_below(0)
+end, { desc = "Comment below" })
+
+vim.keymap.set("n", "gcO", function()
+    comment_above_or_below(-1)
+end, { desc = "Comment above" })
