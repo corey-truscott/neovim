6d8f16e6f63c49e59aa68fb10de537e67ed73011
Corey Truscott <coreytruscott8@gmail.com>
2026-04-03 07:55:56 +0100

    version warning

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
+if vim.fn.has("nvim-0.12.0") == 0 then
+  vim.api.nvim_echo({
+    { "Configuration requires Neovim >= 0.12.0, most stuff will probably break.\n", "ErrorMsg" },
+  }, true, {})
+  vim.fn.getchar()
+  return {}
+end
+
 require("opt.set")
 require("opt.remap")
