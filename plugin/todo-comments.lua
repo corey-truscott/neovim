62b9a786c2598c36f2dc0cd72b987f28caed9d30
Corey Truscott <coreytruscott8@gmail.com>
2026-04-06 04:15:34 +0100

    keybind for todo trouble

--- plugin/todo-comments.lua
+++ plugin/todo-comments.lua
@@ -12,3 +12,5 @@ end, { desc = "Next Todo comment" })
 vim.keymap.set("n", "[t", function()
     require("todo-comments").jump_prev()
 end, { desc = "Previous Todo comment" })
+
+vim.keymap.set("n", "<leader>tt", "<cmd>TodoTrouble toggle<cr>", { desc = "Todo Trouble" })
